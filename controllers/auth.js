c359ce86927d6dfab8cdc9eafd5eb7cad3733e83
DKVAN21 <104008131+DKVAN21@users.noreply.github.com>
2023-12-13 21:18:29 +0700

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { jwtSecret, jwtExpire } = require('../config/keys');
-
+let a = 1;
 exports.signupController = async (req, res) => {
     const { username, email, password } = req.body;
 
